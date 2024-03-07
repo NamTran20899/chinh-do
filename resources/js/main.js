0b453aed08c6681b549d82182de2f5df901a3b30
ductrieu2910 <trieu0892@gmail.com>
2024-03-07 16:26:41 +0700

    feat:create new.html

---
+++ resources/js/main.js
@@ -0,0 +1,28 @@
+/*-- Side Nav --*/
+function openNav() {
+    const sideBarWidth = document.getElementById("Sidenav").style.transform;
+  
+    if (sideBarWidth == "translateX(120px)") {
+      document.getElementById("Sidenav").style.transform = "translateX(0)";
+      document.getElementById("Sidebtn").style.transform = "rotate(0deg)";
+    } else {
+      document.getElementById("Sidenav").style.transform = "translateX(120px)";
+      document.getElementById("Sidebtn").style.transform = "rotate(180deg)";
+    }
+  }
+  
+  /*-- Dropdown --*/
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "grid") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "grid";
+      }
+    });
+  }
