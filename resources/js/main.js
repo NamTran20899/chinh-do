f1d6c707a6023f4776448e5a84e6355966cf1b94
NamTran <phuongnamtran20899@gmail.com>
2024-03-12 16:22:24 +0700

    fix: font-size guide.html

--- resources/js/main.js
+++ resources/js/main.js
@@ -78,3 +78,16 @@ function hamburgerBtn() {
     document.getElementById("hamburger").style.transform = "translateX(0%)";
   }
 }
+
+/*-- Dropdown-Responsive --*/
+const dropdownBtn = document.querySelector(".dropdown-btn");
+const dropdownCaret = document.querySelector(".arrow");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+// add click event to dropdown button
+dropdownBtn.addEventListener("click", () => {
+  // add rotate to caret element
+  dropdownCaret.classList.toggle("arrow-rotate");
+  // add open styles to menu element
+  dropdownContent.classList.toggle("menu-open");
+});
