acd44eff0eb24304a226f05434529802a6ce290e
ductrieu2910 <trieu0892@gmail.com>
2024-03-13 10:08:43 +0700

    feat: code guide handbook news newsdetail

--- resources/js/main.js
+++ resources/js/main.js
@@ -80,14 +80,17 @@ function hamburgerBtn() {
 }
 
 /*-- Dropdown-Responsive --*/
+/*-- Dropdown-Responsive --*/
 const dropdownBtn = document.querySelector(".dropdown-btn");
 const dropdownCaret = document.querySelector(".arrow");
 const dropdownContent = document.querySelector(".dropdown-content");
 
 // add click event to dropdown button
-dropdownBtn.addEventListener("click", () => {
-  // add rotate to caret element
-  dropdownCaret.classList.toggle("arrow-rotate");
-  // add open styles to menu element
-  dropdownContent.classList.toggle("menu-open");
-});
+if (dropdownBtn) {
+  dropdownBtn.addEventListener("click", () => {
+    // add rotate to caret element
+    dropdownCaret.classList.toggle("arrow-rotate");
+    // add open styles to menu element
+    dropdownContent.classList.toggle("menu-open");
+  });
+}
